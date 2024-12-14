eaf93c569de2c1a189fba6e0ccd9e392f5b344ce
MLankaoui <marouanelankaoui66@gmail.com>
2024-12-14 22:27:03 +0100

    add preloader effect.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,36 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Hero from "./components/Hero/Hero";
 import Movies from "./components/movies/Movies";
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+
+    
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setLoading(false); // Hide the spinner after 7 seconds
+        }, 2000);
+
+        // Cleanup timeout if the component unmounts
+        return () => clearTimeout(timeout);
+    }, []);
   return (
     <>
+      {loading ? (
+                <div id="spinner" className="container">
+                    <div className="loading"></div>
+                </div>
+            ) :(
+
       <Router>
         <Routes>
             <Route path="/" element={<Hero />} />
             <Route path="/movies" element={<Movies />} />
         </Routes>
       </Router>
+      )}
     </>
   )
 }
