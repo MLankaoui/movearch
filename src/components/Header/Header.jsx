d5feb3f77415a966a2abf3a6801c901d2f653b77
MLankaoui <marouanelankaoui66@gmail.com>
2024-12-14 19:36:00 +0100

    finish working on the header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,21 @@ const Header = () => {
     return (
         <>
             <div className='navbar-container'>
-                
+                <ul className='navbar'>
+                    <div className='left'>
+                        <li className='nav-logo'>MOVEARCH</li>
+                    </div>
+                    <div className='right'>
+                        <li className='nav-item'>Horror</li>
+                        <li className='nav-item'>Romance</li>
+                        <li className='nav-item'>War</li>
+                    </div>
+
+                    <div className='search'>
+                        <input type='text' className='search-bar' placeholder='The god father'/>
+                        <button className='search-button'>Search</button>
+                    </div>
+                </ul>
             </div>
 
         </>
